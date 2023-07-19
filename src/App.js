e3a631beabb3cdada9f453e0efa4005a582ecbd1
Shreya <shreya27tripathi@gmail.com>
2023-07-19 18:18:57 +0530

    new files

---
+++ src/App.js
@@ -0,0 +1,68 @@
+// import logo from './logo.svg';
+import React, { useEffect } from "react";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './Header';
+import Home from './Home';
+import Login from "./Login";
+import Checkout from "./Checkout";
+import { auth } from "./firebase";
+
+import { useStateValue } from "./StateProvider";
+// import { loadStripe } from "@stripe/stripe-js";
+// import { Elements } from "@stripe/react-stripe-js";
+
+
+function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+    return () =>{
+      unsubscribe();
+    }
+  }, []);
+
+
+  return (
+    <Router>
+      <div className="app">
+      <Routes>
+
+        <Route path="/checkout" element={<><Header/><Checkout/></>}>
+        </Route>
+
+        <Route path="/login" element={<Login/>}>
+        </Route>
+
+        <Route path="/" element={<><Header/><Home/></>}>
+        </Route>
+
+      </Routes>
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import "./Header.css"
+import {Link} from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { auth } from "./firebase";
+function Header() {
+
+  const [{ basket, user }, dispatch] = useStateValue(); 
+  
+
+  const handleAuthenticaton = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
+
+  return (
+    <nav className='header'>
+        <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="Amazon_logo"
+        />
+        </Link>
+        <div className="header__search">
+
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+        
+        </div>
+
+
+        <div className="header__nav">
+        <Link to={!user && '/login'}>
+        
+          <div onClick={handleAuthenticaton} className="header__option"> 
+          {/* <div  className="header__option">  */}
+            <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
+            {<span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span> }
+          </div>
+        </Link>
+
+        
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        
+        
+        <Link to='https://www.primevideo.com/offers/nonprimehomepage/ref=dvm_pds_amz_in_as_s_gm_159_mkw_sRumWk2Nj-dc?gclid=Cj0KCQjw8NilBhDOARIsAHzpbLDvTaVzR7ei7qcZ0rPpPhEET2croPdykfa70r1Mwtfsa2LaEVHhUP8aAgHOEALw_wcB&mrntrk=pcrid_657901934579_slid__pgrid_84577172328_pgeo_1007820_x__adext__ptid_kwd-303629226711' target='_blank'>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+        </Link>
+        
+
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingCartIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
+        </div>
+
+        
+    </nav>
+  )
+}
+
+export default Header
